1a37435f6934a31206eda4910cd31e69bd82bc72
Three Freezen <286973013+Three-Freezen@users.noreply.github.com>
2026-08-22 13:12:16 +0800

    revert: drop v1.1 i18n WIP files (loc extension, seed tool, tests, json)

--- tools/verify-embedded/Program.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Resources;
-var asm = Assembly.LoadFrom(@"D:\BS\项目\DesktopZones\bin\Debug\net10.0-windows\DeskOrder.dll");
-Console.WriteLine($"All {asm.GetManifestResourceNames().Length} top-level resources:");
-foreach (var n in asm.GetManifestResourceNames()) Console.WriteLine($"  {n}");
-Console.WriteLine("\nContents of DeskOrder.g.resources:");
-using var s = asm.GetManifestResourceStream("DeskOrder.g.resources");
-using var rr = new ResourceReader(s!);
-foreach (var k in rr) Console.WriteLine($"  {k}");
